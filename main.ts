7943c96ee2cc1e1276bee301274601e9ffef5e6a
Sonny Andersson <119581514+SonnyAnderssonSe@users.noreply.github.com>
2022-12-01 11:04:28 +0100

    Min och Max visas då knappar trycks ner

--- main.ts
+++ main.ts
@@ -1,7 +1,37 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    basic.showIcon(IconNames.Square)
-    basic.showString("" + (receivedNumber))
+    basic.showString("")
     basic.showString("C")
+    basic.showIcon(IconNames.Square)
+    if (receivedNumber < MinTemp) {
+        MinTemp = receivedNumber
+    }
+    if (receivedNumber > MaxTemp) {
+        MaxTemp = receivedNumber
+    }
 })
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
+    basic.showString("" + (MinTemp))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    basic.showString("" + (MaxTemp))
+})
+let MinTemp = 0
+let MaxTemp = 0
 radio.setGroup(22)
 basic.showIcon(IconNames.Square)
+MaxTemp = input.temperature()
+MinTemp += input.temperature()
